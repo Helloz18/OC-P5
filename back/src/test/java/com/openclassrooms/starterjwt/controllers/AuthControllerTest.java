d30b80e362138a20714dd6950ab49911f71b9c12
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-05-21 10:30:41 +0200

    back: add tests in AuthControllerTest

---
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/AuthControllerTest.java
@@ -0,0 +1,56 @@
+package com.openclassrooms.starterjwt.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.openclassrooms.starterjwt.payload.request.LoginRequest;
+import com.openclassrooms.starterjwt.payload.request.SignupRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper
+            mapper;
+
+    @Test
+    public void whenUserLogInWithCorrectCredentialsThenUserIsLoggedIn() throws Exception {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setEmail("yoga@studio.com");
+        loginRequest.setPassword("test!1234");
+
+        mockMvc.perform(post("/api/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(loginRequest)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Transactional
+    @Rollback
+    public void whenUserRegisterThenUserIsCreated() throws Exception {
+        SignupRequest signupRequest = new SignupRequest();
+        signupRequest.setEmail("test@studio.com");
+        signupRequest.setFirstName("first");
+        signupRequest.setLastName("Last");
+        signupRequest.setPassword("test!1234");
+
+        mockMvc.perform(post("/api/auth/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(signupRequest)))
+                .andExpect(status().isOk());
+    }
+}
