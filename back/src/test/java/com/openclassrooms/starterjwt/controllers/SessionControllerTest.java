c0c7ec4590ed3af6a943e3b3421b3a52c3d9adbf
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-05-21 10:06:44 +0200

    back: add tests in SessionControllerTest

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/SessionControllerTest.java
@@ -14,8 +14,8 @@ import javax.transaction.Transactional;
 import java.util.Date;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
@@ -51,10 +51,22 @@ public class SessionControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
+    public void whenFindAllSessionsThenListOfSessionsIsReturned() throws Exception {
+        // Given there is only one session in the database
+        int numberOfSessions = 1;
+        // When Then
+        mockMvc.perform(get("/api/session/"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(numberOfSessions))
+                .andReturn();
+    }
+
+    @Test
     @Transactional // to have all operations in a unique transaction
     @Rollback // to rollback the modification done to the database
     @WithMockUser(roles="USER")
-    public void whenCreateSession() throws Exception {
+    public void whenCreateSessionThenSessionIsCreated() throws Exception {
         SessionDto sessionDto = new SessionDto();
         sessionDto.setName("session test");
         sessionDto.setTeacher_id(1L);
@@ -64,6 +76,60 @@ public class SessionControllerTest {
         mockMvc.perform(post("/api/session")
                 .contentType(APPLICATION_JSON)
                 .content(mapper.writeValueAsString(sessionDto)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Transactional // to have all operations in a unique transaction
+    @Rollback // to rollback the modification done to the database
+    @WithMockUser(roles="ADMIN")
+    public void whenUpdateSessionByItsIdThenSessionIsUpdated() throws Exception {
+        String id = "1";
+        SessionDto sessionDto = new SessionDto();
+        sessionDto.setName("session test updated");
+        sessionDto.setTeacher_id(1L);
+        sessionDto.setDate(new Date());
+        sessionDto.setDescription("test");
+
+        mockMvc.perform(put("/api/session/" + id)
+                                .contentType(APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(sessionDto)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Transactional // to have all operations in a unique transaction
+    @Rollback // to rollback the modification done to the database
+    @WithMockUser(roles="ADMIN")
+    public void whenDeleteSessionByItsIdThenSessionIsDeleted() throws Exception {
+        String id = "1";
+
+        mockMvc.perform(delete("/api/session/" + id))
+                .andExpect(status().isOk());
+    }
+
+    //{id}/participate/{userId}
+    @Test
+    @Transactional // to have all operations in a unique transaction
+    @Rollback // to rollback the modification done to the database
+    @WithMockUser(roles="USER")
+    public void whenUserParticipateToSessionByItsIdThenSessionIsUpdated() throws Exception {
+        String sessionId = "1";
+        String userId = "1";
+
+        mockMvc.perform(post("/api/session/" + sessionId + "/participate/" + userId))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Transactional // to have all operations in a unique transaction
+    @Rollback // to rollback the modification done to the database
+    @WithMockUser(roles="USER")
+    public void whenUserNoLongerParticipateToSessionByItsIdThenSessionIsUpdated() throws Exception {
+        String sessionId = "1";
+        String userId = "2";
+
+        mockMvc.perform(delete("/api/session/" + sessionId + "/participate/" + userId))
                 .andExpect(status().isOk());
     }
 }
