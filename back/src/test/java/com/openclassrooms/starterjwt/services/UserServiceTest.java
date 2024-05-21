8d773bd6b1083abc65a7aa7dc988ab3af09ffa87
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-05-21 10:56:54 +0200

    back: add tests in UserServiceTest

---
+++ back/src/test/java/com/openclassrooms/starterjwt/services/UserServiceTest.java
@@ -0,0 +1,58 @@
+package com.openclassrooms.starterjwt.services;
+
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    private User user1 = new User();
+
+    @BeforeEach
+    public void createMockUser() {
+        user1.setId(1L);
+        user1.setEmail("user1@test.com");
+        user1.setPassword("test!1234");
+        user1.setLastName("last1");
+        user1.setFirstName("first1");
+    }
+
+    @Test
+    public void testDeleteByIdShoudCallRepositoryWithCorrectId() {
+        // Given
+        Long id = 1L;
+        doNothing().when(userRepository).deleteById(id);
+        // When
+        userService.delete(id);
+        // Then
+        verify(userRepository).deleteById(id);
+    }
+
+    @Test
+    public void whenFindByIdThenUserIsReturned() {
+        // Given
+        Long id = 1L;
+        when(userRepository.findById(id)).thenReturn(Optional.ofNullable(user1));
+        // When
+        User userFound = userService.findById(id);
+        // Then
+        assertEquals("first1", userFound.getFirstName());
+    }
+}
