f296f26442d70dd541329909025e26ae4ca8346e
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-05-22 09:50:53 +0200

    back: add test for main

--- back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
+++ back/src/test/java/com/openclassrooms/starterjwt/SpringBootSecurityJwtApplicationTests.java
@@ -1,13 +1,23 @@
 package com.openclassrooms.starterjwt;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 public class SpringBootSecurityJwtApplicationTests {
 
 	@Test
+	@DisplayName("test that the context is loaded.")
 	public void contextLoads() {
+	}
+
+	@Test
+	@DisplayName("test that the application will be launched without error.")
+	public void testMain() {
+		assertDoesNotThrow(() -> SpringBootSecurityJwtApplication.main(new String[] {}));
 	}
 
 }
