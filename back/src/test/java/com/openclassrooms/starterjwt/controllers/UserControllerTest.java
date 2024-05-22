ee8e72a7b05bb5cdb3daa1e7bf0918bdfc82e2a4
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-05-22 10:11:38 +0200

    back : add annotations in UserControllerTest

--- back/src/test/java/com/openclassrooms/starterjwt/controllers/UserControllerTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/UserControllerTest.java
@@ -1,5 +1,8 @@
 package com.openclassrooms.starterjwt.controllers;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,69 +21,83 @@ public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    @WithMockUser(roles = "USER")
-    public void whenGetUserByIdThenUserIsReturned() throws Exception {
-        // Given
-        Long id = 1L;
-        // When Then
-        mockMvc.perform(get("/api/user/" + id))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.firstName").value("Admin"))
-                .andReturn();
-    }
-    @Test
-    @WithMockUser(roles = "USER")
-    public void whenGetUserdButIdIsNotAlongThenReturnBadRequest() throws Exception {
-        // Given
-        String id = "1L";
-        // When Then
-        mockMvc.perform(get("/api/user/" + id))
-                .andExpect(status().isBadRequest())
-                .andReturn();
+    @Nested
+    @Tag("testGetUserByIdMethod")
+    class TestGetUserById {
+        @Test
+        @DisplayName("get a user with a valid Id.")
+        @WithMockUser(roles = "USER")
+        public void whenGetUserByIdThenUserIsReturned() throws Exception {
+            // Given
+            Long id = 1L;
+            // When Then
+            mockMvc.perform(get("/api/user/" + id))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.firstName").value("Admin"))
+                    .andReturn();
+        }
+        @Test
+        @DisplayName("get a user with an Id of the wrong type.")
+        @WithMockUser(roles = "USER")
+        public void whenGetUserdButIdIsNotAlongThenReturnBadRequest() throws Exception {
+            // Given
+            String id = "1L";
+            // When Then
+            mockMvc.perform(get("/api/user/" + id))
+                    .andExpect(status().isBadRequest())
+                    .andReturn();
+        }
+
+        @Test
+        @DisplayName("get a user with an Id that doesn't exist.")
+        @WithMockUser(roles = "USER")
+        public void whenGetUserByIdButUserIsNullThenReturnNotFound() throws Exception {
+            // Given
+            Long id = 3L;
+            // When Then
+            mockMvc.perform(get("/api/user/" + id))
+                    .andExpect(status().isNotFound())
+                    .andReturn();
+        }
     }
 
-    @Test
-    @WithMockUser(roles = "USER")
-    public void whenGetUserByIdButUserIsNullThenReturnNotFound() throws Exception {
-        // Given
-        Long id = 3L;
-        // When Then
-        mockMvc.perform(get("/api/user/" + id))
-                .andExpect(status().isNotFound())
-                .andReturn();
-    }
+    @Nested
+    @Tag("testDeleteUserMethod")
+    class TestDeleteUserById {
+        @Test
+        @DisplayName("delete a user with a correct Id.")
+        @WithMockUser(roles = "ADMIN")
+        public void whenDeleteUserByIdButUserIsConnectedThenReturnUnauthorized() throws Exception {
+            // Given
+            Long id = 1L;
+            // When Then
+            mockMvc.perform(delete("/api/user/" + id))
+                    .andExpect(status().isUnauthorized())
+                    .andReturn();
+        }
 
-    @Test
-    @WithMockUser(roles = "ADMIN")
-    public void whenDeleteUserByIdButUserIsConnectedThenReturnUnauthorized() throws Exception {
-        // Given
-        Long id = 1L;
-        // When Then
-        mockMvc.perform(delete("/api/user/" + id))
-                .andExpect(status().isUnauthorized())
-                .andReturn();
-    }
+        @Test
+        @DisplayName("delete a user with an Id of the wrong type.")
+        @WithMockUser(roles = "USER")
+        public void whenDeleteUserByIdButIdIsNotAlongThenReturnBadRequest() throws Exception {
+            // Given
+            String id = "1L";
+            // When Then
+            mockMvc.perform(delete("/api/user/" + id))
+                    .andExpect(status().isBadRequest())
+                    .andReturn();
+        }
 
-    @Test
-    @WithMockUser(roles = "USER")
-    public void whenDeleteUserByIdButIdIsNotAlongThenReturnBadRequest() throws Exception {
-        // Given
-        String id = "1L";
-        // When Then
-        mockMvc.perform(delete("/api/user/" + id))
-                .andExpect(status().isBadRequest())
-                .andReturn();
-    }
-
-    @Test
-    @WithMockUser(roles = "USER")
-    public void whenDeleteUserByIdButUserIsNullThenReturnNotFound() throws Exception {
-        // Given
-        Long id = 3L;
-        // When Then
-        mockMvc.perform(delete("/api/user/" + id))
-                .andExpect(status().isNotFound())
-                .andReturn();
+        @Test
+        @DisplayName("delte a user with an Id that doesn't exist.")
+        @WithMockUser(roles = "USER")
+        public void whenDeleteUserByIdButUserIsNullThenReturnNotFound() throws Exception {
+            // Given
+            Long id = 3L;
+            // When Then
+            mockMvc.perform(delete("/api/user/" + id))
+                    .andExpect(status().isNotFound())
+                    .andReturn();
+        }
     }
 }
