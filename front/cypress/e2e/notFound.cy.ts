0c5e8ab718f892d3ba362fe2cbf8b4878b99ea71
Héloïse Finoux-Nikolic <h.finoux-nikolic@ik.me>
2024-06-01 15:01:13 +0200

    add tests for not found page

---
+++ front/cypress/e2e/notFound.cy.ts
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe('not found spec', () => {
+  it('should show not found page if user visit page that does not exist', () => {
+    cy.visit('/unknown');
+
+    // assert
+    cy.url().should('include', '404');
+    cy.get('h1').should('have.text', 'Page not found !');
+  });
+});
